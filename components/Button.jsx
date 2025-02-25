8d66f83e12acc440992f0ef7582a4b0832a72949
Bujujan <bujujan.ru@gmail.com>
2025-02-25 19:26:02 +0000

    changed the files to jsx and not tsx

--- components/Button.jsx
+++ components/Button.jsx
@@ -2,11 +2,9 @@ import { StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-nativ
 import React from 'react'
 import { Link } from 'expo-router'
 
-type ButtonProps = {
-    title: string;
-  };
 
-const Button = ( { title}: ButtonProps) => {
+
+const Button = ( { title }) => {
   return (
     <TouchableOpacity className='bg-blue-600 px-6 py-5 rounded-xl'>
         <Text className='text-white font-psemibold'>{title}</Text>
